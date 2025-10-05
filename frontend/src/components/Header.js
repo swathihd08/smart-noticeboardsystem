8411d8d51846f21fc564b5913af356e8e7e288bf
Thorxthunderer <thor1thunderer@gmail.com>
2025-10-05 21:21:11 +0530

    Update brand name

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <LinkContainer to="/">
-                        <Navbar.Brand>Smart Noticeboard</Navbar.Brand>
+                        <Navbar.Brand>NCE Smart Noticeboard</Navbar.Brand>
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
